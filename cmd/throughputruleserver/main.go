78656d42617b2d859bf6e13b4bd70ae3ef222fbc
alfredlucero <alfred.lucero@ucla.edu>
2021-06-02 15:58:54 -0700

    Added envconfig to throughput rule server

---
+++ cmd/throughputruleserver/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	// "gobrm/throughputrule"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type MySQLConfig struct {
+	User     string
+	Password string
+	Database string
+	Port     int
+}
+
+type ServerConfig struct {
+	Port int
+}
+
+func main() {
+	var mySQLConfig MySQLConfig
+	err := envconfig.Process("mysql", &mySQLConfig)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	log.Printf("MySQL Config: %+v", mySQLConfig)
+
+	var serverConfig ServerConfig
+	err = envconfig.Process("server", &serverConfig)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	log.Printf("Server Config: %+v", serverConfig)
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module gobrm
 go 1.16
 
 require (
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/prometheus/client_golang v1.10.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gorilla/mux v1.8.0
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/prometheus/client_golang v1.10.0
 )
