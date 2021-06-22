f11285d1794c9e43b28244aed36409869689b585
alfredlucero <alfred.lucero@ucla.edu>
2021-06-22 14:39:02 -0700

    Adding id parsing and model functions

--- throughputrule/app.go
+++ throughputrule/app.go
@@ -1,10 +1,12 @@
 package throughputrule
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -12,6 +14,7 @@ import (
 
 type App struct {
 	Router *chi.Mux
+	DB     *sql.DB
 }
 
 func (a *App) Initialize(user, password, dbname string) {
@@ -19,13 +22,12 @@ func (a *App) Initialize(user, password, dbname string) {
 
 	log.Printf("Connecting to %s...", connectionString)
 
-	// TODO: figure out DB stuff with sqlboiler
-	// db, err := sql.Open("mysql", connectionString)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// a.DB = db
+	a.DB = db
 	a.Router = chi.NewRouter()
 	a.Router.Use(middleware.Logger)
 	a.initializeRoutes()
@@ -70,14 +72,32 @@ func (a *App) createThroughputRule(w http.ResponseWriter, r *http.Request) {
 }
 
 func (a *App) getThroughputRule(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid throughput rule ID")
+		return
+	}
+	log.Printf("Getting throughput rule with id %d", id)
 	respondWithJSON(w, http.StatusOK, []string{"throughput1", "throughput2"})
 }
 
 func (a *App) updateThroughputRule(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid throughput rule ID")
+		return
+	}
+	log.Printf("Updating throughput rule with id %d", id)
 	respondWithJSON(w, http.StatusOK, []string{"throughput1", "throughput2"})
 }
 
 func (a *App) deleteThroughputRule(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid throughput rule ID")
+		return
+	}
+	log.Printf("Deleting throughput rule with id %d", id)
 	respondWithJSON(w, http.StatusOK, []string{"throughput1", "throughput2"})
 }
 
@@ -86,5 +106,11 @@ func (a *App) getThroughputRuleChanges(w http.ResponseWriter, r *http.Request) {
 }
 
 func (a *App) getThroughputRuleChangesForThroughputRule(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid throughput rule ID")
+		return
+	}
+	log.Printf("Getting throughput rule changes for throughput rule with id %d", id)
 	respondWithJSON(w, http.StatusOK, []string{"throughputchange1", "throughputchange2"})
 }
--- throughputrule/model.go
+++ throughputrule/model.go
@@ -1,1 +1,57 @@
 package throughputrule
+
+import "database/sql"
+
+// CREATE TABLE throughput_rule (
+//   id INT(10) NOT NULL AUTO_INCREMENT,
+//   mx_domain VARCHAR(255) NOT NULL UNIQUE,
+//   max_connections INT(11) NOT NULL,
+//   messages_per_connection INT(11) NOT NULL,
+//   connection_ttl_millis INT(11) NOT NULL,
+//   PRIMARY KEY(id)
+// );
+
+const throughputRuleTable = "throughput_rule"
+
+func getThroughputRules(db *sql.DB) {
+
+}
+
+func getThroughputRule(db *sql.DB) {
+
+}
+
+func createThroughputRule(db *sql.DB) {
+
+}
+
+func updateThroughputRule(db *sql.DB) {
+
+}
+
+func deleteThroughputRule(db *sql.DB) {
+
+}
+
+// CREATE TABLE throughput_rule_change (
+//   id INT NOT NULL AUTO_INCREMENT,
+//   action VARCHAR(16) NOT NULL,
+//   throughput_rule_id INT(10) NOT NULL,
+//   mx_domain VARCHAR(255) NOT NULL,
+//   max_connections INT(11) NOT NULL,
+//   messages_per_connection INT(11) NOT NULL,
+//   connection_ttl_millis INT(11) NOT NULL,
+//   updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+//   PRIMARY KEY (id),
+//   FOREIGN KEY (throughput_rule_id) REFERENCES throughput_rule(id) ON DELETE CASCADE
+// );
+
+const throughputRuleChangeTable = "throughput_rule_change"
+
+func getThroughputRuleChanges(db *sql.DB) {
+
+}
+
+func getThroughputRuleChangesForThroughputRule(db *sql.DB) {
+
+}
